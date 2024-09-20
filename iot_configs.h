eee5fb0474b48f844489685910f19f0ed26ea2a9
pornphon <pornphon@3dtv-tech.com>
2024-09-20 09:31:48 +0700

    Send temperature and humidity to iOT Hub

---
+++ iot_configs.h
@@ -0,0 +1,14 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// SPDX-License-Identifier: MIT
+
+// Wifi
+#define IOT_CONFIG_WIFI_SSID "3DTVTech_Home"
+#define IOT_CONFIG_WIFI_PASSWORD "gold1234"
+
+// Azure IoT
+#define IOT_CONFIG_IOTHUB_FQDN "iOTHubName-3D.azure-devices.net"
+#define IOT_CONFIG_DEVICE_ID "iotdev1"
+#define IOT_CONFIG_DEVICE_KEY "pNfqTzoADodcRBaGKTv2oXOGr1qR3Yk8GAIoTBMWXfA="
+
+// Publish 1 message every 2 seconds
+#define TELEMETRY_FREQUENCY_MILLISECS 1000
